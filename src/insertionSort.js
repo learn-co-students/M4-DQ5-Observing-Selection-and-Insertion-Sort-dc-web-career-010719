de8fc0c47a4ffd5389537c5a29af7b98f67d049f
canikwe <c.anikwe@gmail.com>
2019-03-18 09:53:40 -0400

    done

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,16 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let i = 1;
+  while (i < arr.length) {
+    let j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      const swap = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = swap;
+      j--;
+    }
+    ++i;
+  }
+  return arr;
 }
